f91403c8c4527912b86aa84ed952a7811d88da96
sujitkc <sujitkc@gmail.com>
2017-06-04 17:05:38 +0530

    added src

---
+++ src/graph/clusters.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+
+import random
+import graph as G
+# create a graph with n nodes, and with density * n * (n - 1) edges.
+# Works similar to create_complete_graph, except that, in the end, it
+# removes a few edges to make the edge density 'density'.
+def create_dense_graph(n, density):
+  g = G.Graph.empty_graph()
+  nodes = []
+  for i in range(n):
+    nodes.append(g.generate_new_node_name())
+
+  pairs = [(n1, n2) for n1 in nodes for n2 in nodes]
+  edges = [G.Edge(n1, 1, n2) for (n1, n2) in pairs if n1 != n2]
+  random.shuffle(edges)
+  if(density != 1):
+    edges = edges[ : int(-((1 - density) * len(edges)))]
+  for e in edges:
+    g.add_edge(e)
+  return g
+
+# create a complete graph with n nodes
+def create_complete_graph(n):
+  return create_dense_graph(n, 1)
+
+# return a graph with len(nlist) complete graphs. Each complete graph has
+# n1, n2 ... nodes where nlist = [n1, n2, ...]
+def create_clusters(nlist, create_graph):
+  g = G.Graph.empty_graph()
+  for n in nlist:
+    newg = create_graph(n)
+    g.merge(newg)
+  return g
+
+# Given a graph g, modify g so that it has some edges (n of them) added to it randomly.
+def add_random_edges(g, n):
+  if(len(g.nodes) < 2):
+    raise Exception("Can't add edges in a graph with fewer than two nodes.")
+  # find non-neighbour node pairs.
+  def find_non_neighbour_pair():
+    nodes = g.nodes.keys()
+    random.shuffle(nodes)
+    i = 0
+    while(i < len(nodes) - 1):
+      (n1, n2) = (nodes[i], nodes[i + 1])
+      if(not g.are_neighbours(n1, n2)):
+        return (n1, n2)
+      i += 1
+    raise Exception("Couldn't find any non-neighbours in this graph.")
+
+  i = 0
+  while(i < n):
+    (n1, n2) = find_non_neighbour_pair()
+    g.add_edge(G.Edge(n1, 1, n2))
+    i += 1
+
+####### test cases ###########
+def t1():
+  g = create_complete_graph(10)
+  print len(g.nodes)
+  print len(g.edges)
+  print(g)
+
+# This test case creates a cluster with 3 dense graphs each with 5, 10, 3 nodes
+# respectively and all with edge density (0.9).
+# Further, it adds 10 edges to the graph.
+def t2():
+  density = 0.9
+  cluster = create_clusters([5, 10, 3], lambda g: create_dense_graph(g, density))
+  print(cluster)
+  print len(cluster.nodes)
+  print len(cluster.edges)
+  add_random_edges(cluster, 10)
+  print len(cluster.nodes)
+  print len(cluster.edges)
+  print(cluster)
+
+if __name__ == "__main__":
+  t1()
+  t2()
---
+++ src/graph/graph.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python
+import random
+import string
+
+# Adjacency List - start
+## Graph - start
+
+class Edge:
+  def __init__(self, n1, w, n2):
+    self._start_ = n1
+    self._weight_  = w
+    self._end_ = n2
+
+  def __str__(self):
+    return "Edge(" + str(self._start_) + "," + str(self._weight_) + "," + str(self._end_) + ")"
+
+  @property 
+  def start_node(self): return self._start_
+  @property
+  def end_node(self):   return self._end_
+  @property
+  def weight(self):     return self._weight_
+
+class Graph:
+  @staticmethod
+  def empty_graph():     return Graph([])
+
+  def __init__(self, edges):
+    self._nodes_ = {}
+    self.add_edge_list(edges)
+
+  @property
+  def nodes(self): return self._nodes_
+
+  @property
+  def edges(self):
+    edges = []
+    for n in self.nodes:
+      for (n1, w) in self.nodes[n]:
+        edges.append(Edge(n1 = n, w = w, n2 = n1))
+    return edges
+
+  def __str__(self):
+    s = "Graph(\n"
+    for e in self.edges:
+      s += "  " + str(e) + ",\n"
+    s += ")"
+    return s
+
+  def are_neighbours(self, n1, n2):
+    if(n1 == n2):
+      return False
+    for n, w in self.nodes[n1]:
+      if n2 == n:
+        return True
+    for n, w in self.nodes[n2]:
+      if n1 == n:
+        return True
+ 
+    return False
+
+  def add_edge(self, e):
+    #_add_edge_to_edge_list adds an edge to a given edge list.
+    # ensures not to add duplicate edges (i.e. edges with identical
+    # start and end nodes; adds the weight in that case.
+    def _add_edge_to_edge_list(el, (n, w)):
+      for i in range(len(el)):
+        (n1, w1) = el[i]
+        if(n1 == n):
+          el[i] = (n, w + w1)
+          return
+      el.append((n, w))
+
+    if(not e.start_node in self.nodes):
+      self.nodes[e.start_node] = []
+    edge_list = self.nodes[e.start_node]
+    _add_edge_to_edge_list(edge_list, (e.end_node, e.weight))
+
+  def add_edge_list(self, el):
+    for e in el:
+      self.add_edge(e)
+
+  def generate_new_node_name(self):
+    while(True):
+      rn = ''.join(random.choice(string.lowercase) for i in range(8))
+      if(rn not in self.nodes):
+        return rn
+ 
+  # merge g into self. self gets modified. g remains unchanged.
+  def merge(self, g):
+     
+    new_node_names = {}
+    def clone_edge(e):
+      if(e.start_node not in new_node_names):
+        new_node_names[e.start_node] = self.generate_new_node_name()
+      if(e.end_node not in new_node_names):
+        new_node_names[e.end_node]   = self.generate_new_node_name()
+      n1 = new_node_names[e.start_node]
+      n2 = new_node_names[e.end_node]
+      w  = e.weight
+      return Edge(n1, w, n2)
+
+    for e in g.edges:
+      self.add_edge(clone_edge(e))
+
+   
+## Graph - end
+# Adjacency List - end
+
+if __name__ == "__main__":
+  e1 = Edge("n1", 100, "n2")
+  e2 = Edge("n3", 100, "n2")
+  e3 = Edge("n1", 200, "n3")
+
+  g = Graph.empty_graph()
+  print g
+  g.add_edge_list([e1, e2, e3])
+  for e in g.edges:
+    print e
